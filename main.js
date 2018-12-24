eacecbdaa204e0f8d7faa5b14582164f62223302
Abimael Santos <juanabimael@gmail.com>
2018-12-24 16:29:41 -0500

    ...

--- main.js
+++ main.js
@@ -8,14 +8,12 @@ function elNombre() {
     let nombre = document.getElementById('entrada').value;
     let resultado = document.getElementById('resultado');
 
-    if(nombre.length < 1) {
+    if (nombre.length < 1) {
         resultado.textContent = 'No hay ninguna entrada.';
-    } else if(typeof nombre != String) {
-        resultado.textContent = 'Eso no es un nombre.';
     } else {
         resultado.textContent = 'Tu nombre es: ' + nombre.cap() + '.';
     }
-}
+};
 
 let evento = document.getElementById('enviar');
 evento.addEventListener('click', elNombre, false);
