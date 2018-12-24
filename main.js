420b02fdfbb3919abd697d7ac908fc123afb18c3
Abimael Santos <juanabimael@gmail.com>
2018-12-24 15:04:35 -0500

    Etapa inicial

---
+++ main.js
@@ -0,0 +1,19 @@
+String.prototype.cap = function(){
+    let str = this;
+
+    return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase();
+};
+
+function elNombre() {
+    let nombre = document.getElementById('entrada').value;
+    let resultado = document.getElementById('resultado');
+
+    if(nombre.length < 1) {
+        resultado.textContent = 'No hay ninguna entrada.';
+    } else {
+        resultado.textContent = 'Tu nombre es: ' + nombre.cap() + '.';
+    }
+}
+
+let evento = document.getElementById('enviar');
+evento.addEventListener('click', elNombre, false);
